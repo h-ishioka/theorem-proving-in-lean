970245661cfda70003509bbc3ef2b1ad59fc4880
Hiroki Ishioka <ishioka0222@gmail.com>
2020-06-09 22:40:22 +0900

    証明を加筆

--- src/ch04.lean
+++ src/ch04.lean
@@ -5,7 +5,32 @@ namespace ex01
 
     variables (α : Type) (p q : α → Prop)
 
-    example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
+    example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+        iff.intro
+        (
+            assume h : ∀ x, p x ∧ q x,
+            show (∀ x, p x) ∧ (∀ x, q x), from
+                and.intro
+                (
+                    show ∀ x, p x, from
+                        assume x,
+                        (h x).left
+                )
+                (
+                    show ∀ x, q x, from
+                        assume x,
+                        (h x).right
+                )
+        )
+        (
+            assume h : (∀ x, p x) ∧ (∀ x, q x),
+            show ∀ x, p x ∧ q x, from
+                assume x,
+                and.intro (h.left x) (h.right x)
+        )
+
+
+
     example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
     example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
 end ex01
