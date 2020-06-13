2aae72a06428f846234a2b5acda36240ce25c6c5
Hiroki Ishioka <ishioka0222@gmail.com>
2020-06-13 15:25:33 +0900

    問題文を追加

---
+++ src/ch04-ex05.lean
@@ -0,0 +1,23 @@
+/-
+5. Prove as many of the identities listed in Section 4.4 as you can.
+-/
+
+open classical
+
+variables (α : Type) (p q : α → Prop)
+variable a : α
+variable r : Prop
+
+example : (∃ x : α, r) → r := sorry
+example : r → (∃ x : α, r) := sorry
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := sorry
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
+
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := sorry
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
+example : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
+example : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
