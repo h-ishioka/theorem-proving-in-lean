4030e4d51e094d082d8d4b6486c893349cdc46f5
Hiroki Ishioka <ishioka0222@gmail.com>
2020-06-26 19:55:11 +0900

    sorryの直前に改行を追加

--- src/ch05-ex01.lean
+++ src/ch05-ex01.lean
@@ -8,29 +8,46 @@ namespace ch03_ex01
     variables p q r : Prop
 
     -- commutativity of ∧ and ∨
-    example : p ∧ q ↔ q ∧ p := sorry
-    example : p ∨ q ↔ q ∨ p := sorry
+    example : p ∧ q ↔ q ∧ p :=
+    sorry
+    example : p ∨ q ↔ q ∨ p :=
+    sorry
 
     -- associativity of ∧ and ∨
-    example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
-    example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+    example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) :=
+    sorry
+    example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) :=
+    sorry
 
     -- distributivity
-    example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
-    example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+    example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+    sorry
+    example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
+    sorry
 
     -- other properties
-    example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-    example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-    example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-    example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-    example : ¬(p ∧ ¬p) := sorry
-    example : p ∧ ¬q → ¬(p → q) := sorry
-    example : ¬p → (p → q) := sorry
-    example : (¬p ∨ q) → (p → q) := sorry
-    example : p ∨ false ↔ p := sorry
-    example : p ∧ false ↔ false := sorry
-    example : (p → q) → (¬q → ¬p) := sorry
+    example : (p → (q → r)) ↔ (p ∧ q → r) :=
+    sorry
+    example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
+    sorry
+    example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+    sorry
+    example : ¬p ∨ ¬q → ¬(p ∧ q) :=
+    sorry
+    example : ¬(p ∧ ¬p) :=
+    sorry
+    example : p ∧ ¬q → ¬(p → q) :=
+    sorry
+    example : ¬p → (p → q) :=
+    sorry
+    example : (¬p ∨ q) → (p → q) :=
+    sorry
+    example : p ∨ false ↔ p :=
+    sorry
+    example : p ∧ false ↔ false :=
+    sorry
+    example : (p → q) → (¬q → ¬p) :=
+    sorry
 end ch03_ex01
 
 namespace ch03_ex02
@@ -38,43 +55,58 @@ namespace ch03_ex02
 
     variables p q r s : Prop
 
-    example : (p → r ∨ s) → ((p → r) ∨ (p → s)) := sorry
-    example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
-    example : ¬(p → q) → p ∧ ¬q := sorry
-    example : (p → q) → (¬p ∨ q) := sorry
-    example : (¬q → ¬p) → (p → q) := sorry
-    example : p ∨ ¬p := sorry
-    example : (((p → q) → p) → p) := sorry
+    example : (p → r ∨ s) → ((p → r) ∨ (p → s)) :=
+    sorry
+    example : ¬(p ∧ q) → ¬p ∨ ¬q :=
+    sorry
+    example : ¬(p → q) → p ∧ ¬q :=
+    sorry
+    example : (p → q) → (¬p ∨ q) :=
+    sorry
+    example : (¬q → ¬p) → (p → q) :=
+    sorry
+    example : p ∨ ¬p :=
+    sorry
+    example : (((p → q) → p) → p) :=
+    sorry
 end ch03_ex02
 
 namespace ch03_ex03
     variables p : Prop
 
-    example : ¬(p ↔ ¬p) := sorry
+    example : ¬(p ↔ ¬p) :=
+    sorry
 end ch03_ex03
 
 namespace ch04_ex01
     variables (α : Type) (p q : α → Prop)
 
-    example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
-    example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
-    example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
+    example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+    sorry
+    example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+    sorry
+    example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+    sorry
 end ch04_ex01
 
 namespace ch04_ex02
     variables (α : Type) (p q : α → Prop)
     variable r : Prop
 
-    example : α → ((∀ x : α, r) ↔ r) := sorry
-    example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
-    example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
+    example : α → ((∀ x : α, r) ↔ r) :=
+    sorry
+    example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r :=
+    sorry
+    example : (∀ x, r → p x) ↔ (r → ∀ x, p x) :=
+    sorry
 end ch04_ex02
 
 namespace ch04_ex03
     variables (men : Type) (barber : men)
     variable  (shaves : men → men → Prop)
 
-    example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : false := sorry
+    example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : false :=
+    sorry
 end ch04_ex03
 
 namespace ch04_ex04
@@ -88,19 +120,30 @@ namespace ch04_ex05
     variable a : α
     variable r : Prop
 
-    example : (∃ x : α, r) → r := sorry
-    example : r → (∃ x : α, r) := sorry
-    example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := sorry
-    example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
+    example : (∃ x : α, r) → r :=
+    sorry
+    example : r → (∃ x : α, r) :=
+    sorry
+    example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r :=
+    sorry
+    example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) :=
+    sorry
 
-    example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := sorry
-    example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
-    example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
-    example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+    example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) :=
+    sorry
+    example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) :=
+    sorry
+    example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) :=
+    sorry
+    example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) :=
+    sorry
 
-    example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
-    example : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
-    example : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
+    example : (∀ x, p x → r) ↔ (∃ x, p x) → r :=
+    sorry
+    example : (∃ x, p x → r) ↔ (∀ x, p x) → r :=
+    sorry
+    example : (∃ x, r → p x) ↔ (r → ∃ x, p x) :=
+    sorry
 end ch04_ex05
 
 namespace ch04_ex06
